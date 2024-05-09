44893132db855858913c69ee29aa77cc24e6ac0b
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-05-09 22:39:19 +0530

    commit

--- src/More Projects Page/components/Index.jsx
+++ src/More Projects Page/components/Index.jsx
@@ -11,7 +11,7 @@ const Index = () => {
         <ul>
           <li><a href="https://manavjain01.github.io/My-Mini-Projects/MainFile/">HTML/CSS/JS Projects</a></li>
           <li><a href="https://manavjain01.github.io/My-React-Projects/#/Body">React Projects</a></li>
-          <li>MERN Projects</li>
+          <li><a href="https://mern-projects-amber.vercel.app/">MERN Projects</a></li>
           <li><a href="https://manavjain01.github.io/MachineLearning/MainFile/#">ML Projects</a></li>
           <li><a href="https://my-java-projects-7a17e.web.app/#">Java Projects</a></li>
           <li><a href="https://manavjain01.github.io/BlockChain-Projects/MainFile/">BlockChain Projects</a></li>
